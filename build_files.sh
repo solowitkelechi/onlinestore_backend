3428eec4b7b0cb35131607e31761964973119617
Solomon Ilo <95635209+solowitkelechi@users.noreply.github.com>
2026-01-11 20:31:41 +0100

    Refactor build_files.sh for better dependency management
    Updated build script to include version check and pip upgrade.

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
-pip install -r requirements.txt
+#!/bin/bash
 
-# make migrations
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic
+echo "-----> Python version check"
+python --version
+
+echo "-----> Upgrading pip (optional but good)"
+python -m pip install --upgrade pip
+
+echo "-----> Installing dependencies"
+python -m pip install -r requirements.txt
+
+echo "-----> Collecting static files"
+python manage.py collectstatic --noinput --clear
+
+echo "Build finished!"
